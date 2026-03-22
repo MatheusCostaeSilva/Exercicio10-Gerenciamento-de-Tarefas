a39e5a570ee49af9fe8d89567db74141f40f8b2b
MatheusCostaeSilva <matheuscostaesilva12@gmail.com>
2026-03-22 15:05:26 -0300

    model criada

---
+++ demo/src/main/java/com/example/demo/models/TarefaModel.java
@@ -0,0 +1,57 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="tb_tarefa")
+public class TarefaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="desc")
+    private String descricao;
+
+    private LocalDate dataVencimento;
+    private Boolean concluida;
+
+    public TarefaModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Boolean getConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(Boolean concluida) {
+        this.concluida = concluida;
+    }
+}
