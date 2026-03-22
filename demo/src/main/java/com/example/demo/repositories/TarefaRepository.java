3c47118b5cdce2196c944a7fbdbca486872a33f4
MatheusCostaeSilva <matheuscostaesilva12@gmail.com>
2026-03-22 15:07:04 -0300

    repository criado

---
+++ demo/src/main/java/com/example/demo/repositories/TarefaRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.TarefaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository<TarefaModel, Long> {
+}
